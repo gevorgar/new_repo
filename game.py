d801ab4081f4823d6b9f5bd50d2aee29c154c3f8
Gevorg <gevorg.ar.23@gmail.com>
2023-06-07 10:10:40 +0300

    update

--- game.py
+++ game.py
@@ -1,9 +1,6 @@
-class Archer:
+class Warrior:
     hp = 100
     attack = 10
-    protection = 6
-    critical_damage = 2
-    critical_chance = 20
 
     def __init__(self, name):
         self.name = name
@@ -20,84 +17,50 @@ class Archer:
     def get_info(self):
         return f'Name: {self.name}, HP: {self.hp}, Attack: {self.attack}'
 
-    def fight(self, other):
-        while other.hp > 0 and self.hp >0:
-            print(f'Hit {self.name} HP: {self.hp}')
-            other.hp -= self.attack
-            print(f'Hit {other.name} HP: {other.hp}')
-            self.hp -= other.attack
-        if self.hp > other.hp:
-            return print(f'Win {self.name} HP: {self.hp}')
-        else:
-            return  print(f'Win {other.name} HP: {other.hp}')
-
-
-class Swordsman:
-    hp = 50
+class Archer(Warrior):
     attack = 5
     protection = 6
     critical_damage = 2
     critical_chance = 20
 
+    def hit(self, other):
+        print(f'Hit {self.name} HP: {self.hp}')
+        other.hp -= self.attack
 
-    def __init__(self, name):
-        self.name = name
 
-    def __eq__(self, other):
-        return self.hp == other.hp
+class Swordsman(Warrior):
+    attack = 5
+    protection = 6
+    critical_damage = 2
+    critical_chance = 20
 
-    def __lt__(self, other):
-        return self.hp < other.hp
+    def hit(self, other):
+        print(f'Hit {self.name} HP: {self.hp}')
+        other.hp -= self.attack
 
-    def __gt__(self, other):
-        return self.hp > other.hp
 
-    def get_info(self):
-        return f'Name: {self.name}, HP: {self.hp}, Attack: {self.attack}'
-
-    def fight(self, other):
-        while other.hp > 0 and self.hp > 0:
-            print(f'Hit {self.name} HP: {self.hp}')
-            other.hp -= self.attack
-            print(f'Hit {other.name} HP: {other.hp}')
-            self.hp -= other.attack
-        if self.hp > other.hp:
-            return print(f'Win {self.name} HP: {self.hp}')
-        else:
-            return print(f'Win {other.name} HP: {other.hp}')
-
-class Magician:
-    hp = 10
+class Magician(Warrior):
     attack = 50
     protection = 6
     critical_damage = 2
     critical_chance = 20
 
-    def __init__(self, name):
-        self.name = name
+    def hit(self, other):
+        print(f'Hit {self.name} HP: {self.hp}')
+        other.hp -= self.attack
 
-    def __eq__(self, other):
-        return self.hp == other.hp
-
-    def __lt__(self, other):
-        return self.hp < other.hp
-
-    def __gt__(self, other):
-        return self.hp > other.hp
-
-    def get_info(self):
-        return f'Name: {self.name}, HP: {self.hp}, Attack: {self.attack}'
+class Game:
 
     def fight(self, other):
-        while other.hp > 0 and self.hp > 0:
-            print(f'Hit {self.name} HP: {self.hp}')
-            other.hp -= self.attack
-            print(f'Hit {other.name} HP: {other.hp}')
-            self.hp -= other.attack
+        if isinstance(Warrior):
+            pass
+
+    def is_fight_end(self, other):
         if self.hp > other.hp:
             return print(f'Win {self.name} HP: {self.hp}')
         else:
             return print(f'Win {other.name} HP: {other.hp}')
+
 
 unit1 = Archer('Player')
 unit2 = Swordsman('player 2')
@@ -105,3 +68,16 @@ unit2 = Swordsman('player 2')
 
 print(unit2 > unit1)
 unit1.fight(unit2)
+
+
+# def fight(self, other):
+#     if isinstance(Warrior):
+#         while other.hp > 0 and self.hp > 0:
+#             print(f'Hit {self.name} HP: {self.hp}')
+#             other.hp -= self.attack
+#             print(f'Hit {other.name} HP: {other.hp}')
+#             self.hp -= other.attack
+#         if self.hp > other.hp:
+#             return print(f'Win {self.name} HP: {self.hp}')
+#         else:
+#             return print(f'Win {other.name} HP: {other.hp}')
